c5c332b3dddc2d5ee60943212647f5ba7eabc009
Renthubber <admin@renthubber.com>
2026-01-09 23:48:15 +0100

    Delete lib/supabase.ts

--- lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from "@supabase/supabase-js";
-
-// Credenziali fornite
-const SUPABASE_URL = "https://upyznglekmynztmydtxi.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVweXpuZ2xla215bnp0bXlkdHhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MTQyMjIsImV4cCI6MjA3OTI5MDIyMn0.SsXFHjInQ7bihDEmSwLNdoC-Ap9D4K66QmxUwm7kur8";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
