cb4243155939a66175c6bb08a8e4914a997d1807
Renthubber <admin@renthubber.com>
2026-01-09 23:50:09 +0100

    Add files via upload

--- components/admin/AdminCMSBranding.tsx
+++ components/admin/AdminCMSBranding.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "../../services/supabaseClient";
 import {
   Globe,
   Palette,
