3eb65533ce45f67163cbd5b18bba932610f3711d
Renthubber <admin@renthubber.com>
2026-01-11 13:14:23 +0100

    Add files via upload

--- views/Home.tsx
+++ views/Home.tsx
@@ -572,12 +572,12 @@ useEffect(() => {
         <div className="relative">
           <MapPin className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
           <input
-            type="text"
-            value={searchCity}
-            onChange={(e) => setSearchCity(e.target.value)}
-            placeholder="Ovunque"
-            className="w-full pl-10 pr-4 py-3 border border-gray-200 rounded-xl focus:ring-2 focus:ring-brand focus:border-transparent outline-none"
-          />
+  type="text"
+  value={searchCity}
+  onChange={(e) => setSearchCity(e.target.value)}
+  placeholder="Ovunque"
+  className="w-full pl-10 pr-4 py-3 border border-gray-200 rounded-xl focus:ring-2 focus:ring-brand focus:border-transparent outline-none text-gray-800"
+/>
         </div>
         {/* Suggerimenti città */}
         {citySuggestions.length > 0 && (
