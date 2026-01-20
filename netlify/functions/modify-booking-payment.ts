008f9f093f5a086b4f63ae880cc18ffa433c140a
Renthubber <admin@renthubber.com>
2026-01-20 10:12:28 +0100

    Add files via upload

--- netlify/functions/modify-booking-payment.ts
+++ netlify/functions/modify-booking-payment.ts
@@ -40,8 +40,8 @@ export const handler: Handler = async (event, context) => {
 
   try {
     // Crea client Supabase
-    const SUPABASE_URL = process.env.SUPABASE_URL!;
-    const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
+    const SUPABASE_URL = process.env.VITE_SUPABASE_URL!;
+    const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY!;
     const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY!;
     
     const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
